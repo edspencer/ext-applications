8c26302e5b7aac9a7f5313713519e7eadf1b61e2
Edward Spencer <edward@midlandswebdesign.com>
2009-01-23 13:45:06 +0000

    Added a related product chooser to ProductManager

---
+++ ProductManager/views/_RelatedProductAssociator.js
@@ -0,0 +1,34 @@
+/**
+ * Ext.ux.App.ProductManager.view.RelatedProductAssociator
+ * @extends Ext.ux.App.view.HABTMTree
+ * Links any model to one or more categories in a tree
+ */
+Ext.ux.App.ProductManager.view.RelatedProductAssociator = function(config) {
+  var config = config || {};
+  
+  this.objectId            = config.objectId;
+  this.relatedProductModel = Ext.ux.App.ProductManager.Product;
+  
+  Ext.applyIf(config, {
+    title:      "Related Products",
+    height:     315,
+    habtmModel: this.relatedProductModel,
+    model:      Ext.ux.App.ProductManager.Product
+  });
+  
+  Ext.ux.App.ProductManager.view.RelatedProductAssociator.superclass.constructor.call(this, config);
+};
+
+Ext.extend(Ext.ux.App.ProductManager.view.RelatedProductAssociator, Ext.ux.App.view.HABTMTree, {
+  
+  defaultLoaderUrl: function() {
+    if (this.objectId) {
+      return this.relatedProductModel.treeUrl() + '?checked=true&associated_model=' + this.model.model_name + '&associated_id=' + this.objectId;
+    } else {
+      return this.relatedProductModel.treeUrl() + '?checked=true';
+    };
+  }
+  
+});
+
+Ext.reg('related_product_associator', Ext.ux.App.ProductManager.view.RelatedProductAssociator);
