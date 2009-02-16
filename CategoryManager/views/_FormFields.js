36437ec926786c58eccec1ffa7fabd80eb2b7a8e
Edward Spencer <edward@midlandswebdesign.com>
2009-02-16 15:57:05 +0000

    Removed old category manager

--- CategoryManager/views/_FormFields.js
+++
@@ -1,7 +0,0 @@
-Ext.ux.App.CategoryManager.view.FormFields = function() {
-  return [
-    { fieldLabel: 'Title',       name: 'category[title]'},
-    { fieldLabel: 'Description', name: 'category[description]'},
-    { fieldLabel: 'Url Name',    name: 'category[url_param]'}
-  ];
-};
--- CategoryManager/views/_GridColumns.js
+++
@@ -1,9 +0,0 @@
-Ext.ux.App.CategoryManager.view.GridColumns = function() {
-  return [
-    { header: 'Title',       dataIndex: 'title',       type: 'string'},
-    { header: 'Created at',  dataIndex: 'created_at',  type: 'string', hidden: true},
-    { header: 'Updated at',  dataIndex: 'updated_at',  type: 'string', hidden: true},
-    { header: 'Description', dataIndex: 'description', type: 'string'},
-    { header: 'Url Name',    dataIndex: 'url_param',   type: 'string'}
-  ];
-};
